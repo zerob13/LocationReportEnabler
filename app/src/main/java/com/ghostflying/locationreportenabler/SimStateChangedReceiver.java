8711fa965b56fde9e08c415867402867b3ce572d
GhostFlying <GhostFlying@Gmail.com>
2014-12-08 16:57:53 +0800

    change to receive sim state change

--- app/src/main/java/com/ghostflying/locationreportenabler/SimStateChangedReceiver.java
+++ app/src/main/java/com/ghostflying/locationreportenabler/SimStateChangedReceiver.java
@@ -3,24 +3,26 @@ package com.ghostflying.locationreportenabler;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.telephony.TelephonyManager;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class BootCompletedReceiver extends BroadcastReceiver {
+public class SimStateChangedReceiver extends BroadcastReceiver {
     private final String COMMAND_PREFIX = "setprop ";
     private final String[] PROPERTIES = {"gsm.sim.operator.numeric 310004",
             "gsm.sim.operator.iso-country us",
             "gsm.sim.operator.alpha Verizon"};
 
-    public BootCompletedReceiver() {
+    public SimStateChangedReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        enableLocationReport();
-        Log.i("Enabler", "Done.");
+        TelephonyManager manager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (manager.getSimState() == TelephonyManager.SIM_STATE_READY){
+            enableLocationReport();
+        }
     }
 
     public void enableLocationReport() {
