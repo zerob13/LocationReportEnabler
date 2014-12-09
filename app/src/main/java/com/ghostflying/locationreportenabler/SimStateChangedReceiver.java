df057f4b30ad3ea4af5e54ab2c4abb7f0fa1b2fc
GhostFlying <GhostFlying@Gmail.com>
2014-12-09 13:22:07 +0800

    try to change numeric only

--- app/src/main/java/com/ghostflying/locationreportenabler/SimStateChangedReceiver.java
+++ app/src/main/java/com/ghostflying/locationreportenabler/SimStateChangedReceiver.java
@@ -10,9 +10,7 @@ import java.io.IOException;
 
 public class SimStateChangedReceiver extends BroadcastReceiver {
     private final String COMMAND_PREFIX = "setprop ";
-    private final String[] PROPERTIES = {"gsm.sim.operator.numeric 310030",
-            "gsm.sim.operator.iso-country us",
-            "gsm.sim.operator.alpha AT"};
+    private final String[] PROPERTIES = {"gsm.sim.operator.numeric 310030"};
 
     public SimStateChangedReceiver() {
     }
